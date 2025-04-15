86e44f89436267b79e83269a5a39c4fb70966f10
Pruthviraj Urankar <pruthvirajurankar@yahoo.in>
2025-04-15 09:25:16 -0700

    initial draft complete

--- code/photoProof.sol
+++ code/photoProof.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/
+ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PhotoProof is ERC721URIStorage, Ownable {
